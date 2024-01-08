9c86989843d1cb3a96a21823baa96cb0bc5b3150
Deep Tejaswi Adireddy <119724015+DeepTejaswi315@users.noreply.github.com>
2024-01-08 10:32:29 +0530

    import java.util.*;
    public class Solution {
    
        public static void main(String[] args) {
            Scanner scan = new Scanner(System.in);
            double a = scan.nextDouble();
            double b = scan.nextDouble();
            
            System.out.printf("%.2f",a*b);
            
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        
+        System.out.printf("%.2f",a*b);
+        
+    }
+}
