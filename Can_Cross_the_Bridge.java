ae00d123c2d9370ac8c3d795c540943b6588788c
Deep Tejaswi Adireddy <119724015+DeepTejaswi315@users.noreply.github.com>
2024-01-10 17:35:08 +0530

    import java.util.*; public class abhi{     public static void main(String[] args)     {         Scanner s = new Scanner(System.in);         int x = s.nextInt();         int y = s.nextInt();         int z = s.nextInt();         int d = (z-y)/x;         System.out.printf("%d",d);     } }

---
+++ Can_Cross_the_Bridge.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class abhi{
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int z = s.nextInt();
+        int d = (z-y)/x;
+        System.out.printf("%d",d);
+    }
+}
