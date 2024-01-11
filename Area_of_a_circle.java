8c6455cdbdbc5dc47a76b54d094aae11bc99dcef
Deep Tejaswi Adireddy <119724015+DeepTejaswi315@users.noreply.github.com>
2024-01-11 21:26:16 +0530

    import java.util.*; public class Solution {
        public static void main(String[] args) {
            Scanner scan = new Scanner(System.in);
            int r = scan.nextInt();
            double area=3.14*r*r;
            System.out.printf("%.2f",area);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int r = scan.nextInt();
+        double area=3.14*r*r;
+        System.out.printf("%.2f",area);
+    }
+}
